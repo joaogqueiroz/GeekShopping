5fb720da420c87d4f76810ce6b5c68cebd815e72
joaogqueiroz <joaogilberto350@hotmail.com>
2023-05-06 15:04:10 -0300

    feat: preparing product web layer to consumer product microsservice.

---
+++ GeekShopping.Web/Services/ProductService.cs
@@ -0,0 +1,66 @@
+using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+
+namespace GeekShopping.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _client;
+        public const string BasePath = "api/v1/product";
+
+        public ProductService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(HttpClient));
+        }
+        public async Task<IEnumerable<ProductModel>> FindAllProducts()
+        {
+            var response = await _client.GetAsync(BasePath);
+            return await response.Content.ReadFromJsonAsync<List<ProductModel>>();
+        }
+
+        public async Task<ProductModel> FindProductById(long id)
+        {
+            var response = await _client.GetAsync($"{BasePath}/{id}");
+            return await response.Content.ReadFromJsonAsync<ProductModel>();
+        }
+
+        public async Task<ProductModel> CreateProduct(ProductModel model)
+        {
+            var response = await _client.PostAsJsonAsync(BasePath, model);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<ProductModel>();
+            }
+            else
+            {
+                throw new Exception("Something went wrong when calling API");
+            }
+        }
+
+        public async Task<ProductModel> UpdateProduct(ProductModel model)
+        {
+            var response = await _client.PutAsJsonAsync(BasePath, model);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<ProductModel>();
+            }
+            else
+            {
+                throw new Exception("Something went wrong when calling API");
+            }
+        }
+
+        public async Task<bool> DeleteProductById(long id)
+        {
+            var response = await _client.DeleteAsync($"{BasePath}/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<bool>();
+            }
+            else
+            {
+                throw new Exception("Something went wrong when calling API");
+            }
+        }
+    }
+}
